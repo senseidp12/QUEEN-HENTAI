4237f8e66d941d7544a02a11d18aae8ff8e7c353
senseidp12 <152052577+senseidp12@users.noreply.github.com>
2023-11-26 19:29:12 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94767939688', 'Dinuwa Official²⁰²³', true],
-  ['94767939688', 'Dinuwa Official²⁰²³', true], 
+  ['94705031527', 'Dinuwa Official²⁰²³', true],
+  ['94705031527', 'Dinuwa Official²⁰²³', true], 
   [''] 
 ] //Number of owners
 
